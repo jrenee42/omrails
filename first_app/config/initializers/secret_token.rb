a820cc9036f3350d7fb9efaec6ab42f041337b6c
Jill Singer <jillrenee42@gmail.com>
2013-03-17 19:19:43 -0400

    initial commit trying to use vim.....

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e948b8dc3ea8bce7cc94e9f37aaffdafe61b733dd4ae75f8858428ab18c5da5783f6e7f7bde0ea759fb4203c5b0840046cad4ae6f0d799455af85d5878958cdb'
